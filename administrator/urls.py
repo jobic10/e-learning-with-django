d525db53c7d245a40e3a8724907caf54620fb86f
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-06-16 14:34:03 +0100

    Add Department Completed

--- administrator/urls.py
+++ administrator/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name="adminDashboard"),
     path('course/add', views.add_course, name="add_course"),
+    path('department/add', views.add_department, name="add_department"),
 ]
--- administrator/views.py
+++ administrator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import *
+from django.contrib import messages
 # Administrative Functions
 
 
@@ -17,4 +18,29 @@ def add_course(request):
     context = {
         'form': form
     }
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Course Added")
+            context['form'] = AddCourseForm()
+        else:
+            messages.error(request, "Invalid Data Provided")
+    context['courses'] = Course.objects.all()
+
     return render(request, path('course'), context)
+
+
+def add_department(request):
+    form = AddDepartmentForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Department Added")
+            context['form'] = AddDepartmentForm()
+        else:
+            messages.error(request, "Invalid Data Provided")
+    context['departments'] = Department.objects.all()
+    return render(request, path('department'), context)
