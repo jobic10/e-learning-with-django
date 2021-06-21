e132a800186b9921f5d650e9455f3678924399db
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-06-21 17:27:27 +0100

    Staff Model

--- staff/models.py
+++ staff/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from administrator.models import *
 # Create your models here.
+
+
+class Staff(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
