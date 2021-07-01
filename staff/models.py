f1830ce4842354cac960e1200ff7655ba855bfcc
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-01 19:19:31 +0100

    Course Allocation Model

--- staff/models.py
+++ staff/models.py
@@ -7,3 +7,10 @@ class Staff(models.Model):
     department = models.ForeignKey(
         Department, on_delete=models.SET_NULL, null=True)
     admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+
+class CourseAllocation(models.Model):
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, null=True, on_delete=models.SET_NULL)
+    approved = models.BooleanField(default=False)  # * Approved by admin
