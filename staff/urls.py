c4b420e4429e68d9c6f5f957bc4c2200320b30d5
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-06 12:42:33 +0100

    URL

--- staff/urls.py
+++ staff/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name="staffDashboard"),
     path('course/allocation', views.courseAllocation, name="courseAllocation"),
+    path('course/reg/status', views.courseStatus, name="courseStatus"),
+    path('course/application/response/<int:this_id>/<str:status>',
+         views.courseAppResponse, name='courseAppResponse'),
 
 ]
