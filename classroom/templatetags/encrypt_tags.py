79ce26ca4b06a96919790b8af99720a2dbdb0fa0
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-09 21:49:40 +0100

    New tags

---
+++ classroom/templatetags/encrypt_tags.py
@@ -0,0 +1,8 @@
+from django import template
+from e_learning.functions import encrypt as e, decrypt as d
+register = template.Library()
+
+
+@register.filter(name='encrypt')
+def encrypt(value):
+    return e(value)
