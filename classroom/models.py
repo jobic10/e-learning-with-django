19c8451959a5997591803c088cd2fd75c11fcf49
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-09 20:34:43 +0100

    Classroom models

--- classroom/models.py
+++ classroom/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from administrator.models import Session, Course
 from student.models import Student
+from account.models import CustomUser
 from django.core.validators import MinValueValidator
 from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
@@ -21,3 +22,15 @@ class Submission(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     answer = RichTextUploadingField()
     submission_date = models.DateTimeField(auto_now_add=True)
+
+
+class Stream(models.Model):
+    message = RichTextUploadingField()
+    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class StreamReply(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    message = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
