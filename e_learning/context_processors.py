bcf4ab56a5eed582027ddae91238f2f1cc3d9252
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-21 15:44:35 +0100

    App Name Fixed

--- e_learning/context_processors.py
+++ e_learning/context_processors.py
@@ -1,8 +1,8 @@
-from administrator.models import Settings
 from .functions import get_session
+from django.conf import settings
 
 
 def SESSION(request):
     session = get_session()
-    context = {'ACADEMIC_SESSION': session}
+    context = {'ACADEMIC_SESSION': session, 'APP_NAME': settings.APP_NAME}
     return context
--- e_learning/settings.py
+++ e_learning/settings.py
@@ -190,3 +190,5 @@ CKEDITOR_CONFIGS = {
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+APP_NAME = "E-Learning"
