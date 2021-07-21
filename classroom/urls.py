674306de68460a229fdabcd4d89992dbf1b457d1
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-21 15:28:37 +0100

    Code cleanup. Fixed Issues with PR that removed files

--- classroom/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-# from . import student_views, staff_views, views
-from staff import views as v
-urlpatterns = [
-    path('/idontevenknow/', v.courseAllocation, name="wahala"),
-]
--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from classroom import student_views
 
 
 urlpatterns = [
